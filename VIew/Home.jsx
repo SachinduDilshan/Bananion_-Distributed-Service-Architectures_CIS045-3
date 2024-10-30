ccdc40f56da83028c05d0a1d04416c8427ccfaf8
K.G.S.D Abeyrathne <sachindud498@gmail.com>
2024-10-30 07:40:23 +0530

    commit

--- VIew/Home.jsx
+++ VIew/Home.jsx
@@ -58,26 +58,40 @@ const Home = () => {
 
 
   return (
-    <div className="home-container">
+    <div className="container d-flex flex-column justify-content-center align-items-center min-vh-100">
+      {userData ? (
+            <>
+            <div className='welcome-text'>
+            <h1>Welcome, {userData.name}!</h1>
+            </div>
+            </>
+          ) : (
+            <p></p>
+          )}
       <div className="home-content">
         <button className="custom-btn" onClick={() => navigate('/play')}>Let's Play!</button>
         <button className="custom-btn" onClick={() => navigate('/ranks')}>Top Ranks</button>
         <button className="custom-btn" onClick={handleLogout}>Log Out</button>
+        
 
         <div className="profile-section">
           {userData ? (
             <>
-            <h1>Welcome, {userData.name}!</h1>
               <img src={userData.profilePicUrl} alt="User Profile" className="profile-pic" />
               <p>{userData.username}</p>
               <button className="btn profile-btn">My Profile</button>
             </>
           ) : (
-            <p>Loading user data...</p>
+            <p></p>
           )}
         </div>
-      </div>
+        
+      </div>  
+       
+      <h6 className="myName">K.G.S.D. Abeyrathne | 2425049 </h6>
     </div>
+   
+    
   );
 };
 
