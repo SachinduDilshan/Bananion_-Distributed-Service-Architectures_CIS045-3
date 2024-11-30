d5f2f5f4b99ebaf8ddff13b08714c338514b96ad
K.G.S.D Abeyrathne <sachindud498@gmail.com>
2024-11-30 21:44:27 +0530

    commit

--- Controller/TriviaController.js
+++ Controller/TriviaController.js
@@ -2,17 +2,31 @@ import { useState, useEffect } from 'react';
 import triviaFacts from '../Model/TriviaModel.js';
 
 const useTriviaController = () => {
-  const [currentFactIndex, setCurrentFactIndex] = useState(0);
+    const [currentFact, setCurrentFact] = useState("");
+    const [defaultFactIndex, setDefaultFactIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentFactIndex((prevIndex) => (prevIndex + 1) % triviaFacts.length);
-    }, 7000); //changes fact for every 7s
+    useEffect(() => {
+        const fetchTrivia = async () => {
+            try {
+                const response = await fetch("http://numbersapi.com/random/math");
+                const fact = await response.text();
+                setCurrentFact(fact);
+            } catch (error) {
+                console.error("Error fetching trivia:", error);
+                // If API fails, use default trivia facts
+                setCurrentFact(triviaFacts[defaultFactIndex]);
+                setDefaultFactIndex((prevIndex) => (prevIndex + 1) % triviaFacts.length);
+            }
+        };
 
-    return () => clearInterval(interval);
-  }, []);
+        // Fetch a new trivia fact every 7 seconds
+        fetchTrivia();
+        const interval = setInterval(fetchTrivia, 7000);
 
-  return triviaFacts[currentFactIndex];
+        return () => clearInterval(interval);
+    }, [defaultFactIndex]);
+
+    return currentFact;
 };
 
 export default useTriviaController;
--- VIew/MathTrivia.jsx
+++ VIew/MathTrivia.jsx
@@ -1,8 +1,7 @@
-// views/MathTrivia.jsx
 import React from 'react';
 import useTriviaController from '../Controller/TriviaController.js';
 import '../VIew/Styles/Mathrivia.css';
-import picture from './Styles/assets/funfact.png'
+import picture from './Styles/assets/funfact.png';
 
 const MathTrivia = () => {
     const currentFact = useTriviaController();
@@ -10,7 +9,7 @@ const MathTrivia = () => {
     return (
         <div className="math-trivia">
             <img src={picture} alt="Fun Fact" className="fun-pic" />
-            <p>{currentFact} </p>
+            <p>{currentFact || "Loading trivia..."} </p>
         </div>
     );
 };
